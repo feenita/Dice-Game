d82d60156f4135fc786cf9e718869bcce99dd3df
feenita <72655632+feenita@users.noreply.github.com>
2022-11-01 19:53:36 +0530

    Initial file upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var i1="images/dice"+randomNumber1+".png";
+document.querySelectorAll("img")[0].setAttribute("src",i1);;
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var i2="images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",i2);
+
+if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML="Draw!";
+}
+else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML="Player 2 Wins!🚩";
+}
+
+
+
+
+
